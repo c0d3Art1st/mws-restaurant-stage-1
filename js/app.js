76cea79be63d2d2c862811e860b55bf2390c853d
A. Stiegler <stiegler.developer@gmail.com>
2018-03-21 12:30:26 +0100

    refactor: move serviceWorker to app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
    navigator.serviceWorker.register("/sw.js").then(reg =>{
-      console.log("[ServiceWorker] Successfully registed SW");
+      console.log("[ServiceWorker] Successfully registered SW");
    })
    .catch(err => {
       console.log("[ServiceWorker] Error registering SW");
