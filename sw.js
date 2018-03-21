db796edfb05544c931a65a803d032a0a3b6af016
A. Stiegler <stiegler.developer@gmail.com>
2018-03-21 14:37:10 +0100

    feat: implement 'cache first network fallback'-caching-strategy

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 let self = this;
-const STATIC_CACHE_VERSION = 1;
+const STATIC_CACHE_NAME = "static-v1";
+const DYN_CACHE_NAME = "dynamic-v1";
 
 let staticFiles = [
    "/",
@@ -14,7 +15,7 @@ let staticFiles = [
 self.addEventListener('install', event => {
    console.log("[ServiceWorker] Successfully installed SW")
    event.waitUntil(
-      caches.open('static-v' + STATIC_CACHE_VERSION)
+      caches.open(STATIC_CACHE_NAME)
       .then(cache => {
          console.log("[ServiceWorker] Caching static files");
          return cache.addAll(staticFiles);
@@ -23,4 +24,28 @@ self.addEventListener('install', event => {
          console.log("#ERROR# [ServiceWorker] Error while caching static files: ", err);
       })
    );
+});
+
+
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		caches.match(event.request)
+		.then(response => {
+			if (response)
+				return response;
+			return fetch(event.request)
+			.then(res => {
+				return caches.open(DYN_CACHE_NAME)
+				.then(cache => {
+					// store clone in cache and return response
+					cache.put(event.request.url, res.clone());
+					return res;
+				})
+				.catch(err => {
+					// log error or return offline fallback page
+               console.log("#ERROR# [ServiceWorker] Error while caching dynamic files: ", err);
+				});
+			});
+		})
+	);
 });
